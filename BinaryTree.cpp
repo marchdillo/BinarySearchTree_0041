54e6ef740e80b697d135ef68c5f6cb6bc365d69b
marchdillo <kevinno.limiy@gmail.com>
2025-06-26 12:18:39 +0700

    Menambahkan x.preorder(x.ROOT)

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -154,5 +154,11 @@ int main()
             x.inorder(x.ROOT);
             break;
         }
+        case '3':
+        {
+
+            x.preorder(x.ROOT);
+            break;
+        }
         }
     }
